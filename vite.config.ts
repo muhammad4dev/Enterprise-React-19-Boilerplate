865edf0f06bbfc63a8aeccf09ef0139f7fb0929f
Muhammad Mustafa <muhammadidev@outlook.com>
2025-12-01 14:00:00 +0200

    chore: setup vite and react

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", { target: "19" }]],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
